5b9351bd32c0b39b2fc80cea4141096883f82f95
Beau Horsman <60739385+Beauhorsman@users.noreply.github.com>
2026-04-09 16:49:30 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend werkt 🚀");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running 🚀");
+});
